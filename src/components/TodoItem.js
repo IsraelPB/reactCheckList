f809fbfb520db9925bc479aaed9d1c82f3d32d85
MrInP <isra_pena@yahoo.com>
2019-03-10 06:26:01 -0700

    more components stuff

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+
 export class TodoItem extends Component {
     getStyle =() => {
-        if(this.props.todo.completed){
-            return{
-                textDecoration :'line-through'
-            }
-            
-        }
-        else{
-            return{
-                textDecoration :'none'
-            }
-
+        return {
+          background:'#f4f4f4',
+          padding:'10px',
+          borderBottom:'1px #ccc dotted',
+          textDecoration : this.props.todo.completed ?
+          'line-through':'none'
         }
     }
+   
   render() {
+    const {id, title} = this.props.todo;
     return (
       <div style={this.getStyle()}>
-        <p>{this.props.todo.title}</p>
+        <p>
+        <input type="checkbox" onChange={this.props.markComplete.bind(this,id)}/> {' '}
+        {title}</p>
       </div>
     )
   }
