b653bd24a4faaa7e47796f96607315fca209fca7
MrInP <isra_pena@yahoo.com>
2019-03-10 07:35:51 -0700

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import Header from './components/layout/Header';
+import uuid from 'uuid';
 
 class App extends Component {
   state ={
     todos: [
       {
-        id:1,
+        id:uuid.v4(),
         title:'take out the trash',
         completed: false
       },
       {
-        id:2,
+        id:uuid.v4(),
         title:'wash the dogs',
         completed: false
       },
       {
-        id:3,
+        id:uuid.v4(),
         title:'walk the dogs',
         completed: false
       }
@@ -34,13 +36,24 @@ class App extends Component {
   //Delete Todo
   delTodo = (id)=> {
     this.setState({ todos: [...this.state.todos.filter(todo =>todo.id !==id)]})
+  }//Add Todo
+  addTodo=(title)=>{
+    const newTodo ={
+      id:uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({todos: [...this.state.todos, newTodo]})
   }
   render() {
    
     return (
       <div className="App">
+      <div className="container">
       <Header/>
+      <AddTodo addTodo={this.addTodo}/>
       <Todos todos={this.state.todos} markComplete ={this.markComplete} delTodo={this.delTodo}/>
+      </div>
       </div>
     );
   }
